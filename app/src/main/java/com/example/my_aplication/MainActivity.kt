b7a94cae2270c4cc125958f8465ef418e6f8d228
donykurniawan <donykurniawan1298@gmail.com>
2025-12-16 15:58:57 +0700

    + catatan dan tugas xml

--- app/src/main/java/com/example/my_aplication/MainActivity.kt
+++ app/src/main/java/com/example/my_aplication/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_catatan, R.id.navigation_plus, R.id.navigation_tugas
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
