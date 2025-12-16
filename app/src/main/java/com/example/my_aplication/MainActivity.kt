98bbd9f8e65d7e16d76dbf2e7bfff62bde6a1794
Nick17123 <mahardiannicholas@gmail.com>
2025-12-16 19:22:48 +0700

    perbaikabn warna dan fragmencontainer serta reset kode MainActivity.kt

--- app/src/main/java/com/example/my_aplication/MainActivity.kt
+++ app/src/main/java/com/example/my_aplication/MainActivity.kt
@@ -1,35 +1,15 @@
 package com.example.my_aplication
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.my_aplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_catatan, R.id.navigation_plus, R.id.navigation_tugas
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
